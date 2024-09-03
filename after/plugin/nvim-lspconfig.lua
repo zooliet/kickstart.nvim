a7ed10d2b7bfe221cdd1a83cfc93a21038721e1a
Junhyun Shin <hl1sqi@gmail.com>
2024-09-03 14:04:57 +0900

    change diagnotic colors

--- after/plugin/nvim-lspconfig.lua
+++ after/plugin/nvim-lspconfig.lua
@@ -1,21 +1,27 @@
--- vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
---   -- virtual_text = true,
---   virtual_text = { spacing = 4, prefix = ''}, -- or false
---   signs = true,
---   underline = false,
---   update_in_insert = false,
--- })
---
--- local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
---
--- for type, icon in pairs(signs) do
---   local hl = "DiagnosticSign" .. type
---   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
--- end
---
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  -- virtual_text = true,
+  virtual_text = { spacing = 4, prefix = '' }, -- or false
+  signs = true,
+  underline = false,
+  update_in_insert = false,
+})
+
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.cmd [[
+  highlight DiagnosticVirtualTextError guifg=#666666
+  highlight DiagnosticVirtualTextWarn guifg=#666666
+  highlight DiagnosticVirtualTextHint guifg=#666666
+  highlight DiagnosticVirtualTextInfo guifg=#666666
+]]
 -- vim.cmd [[
---   highlight DiagnosticVirtualTextError guifg=#666666
---   highlight DiagnosticVirtualTextWarn guifg=#666666
---   highlight DiagnosticVirtualTextHint guifg=#666666
---   highlight DiagnosticVirtualTextInfo guifg=#666666
+--   highlight DiagnosticVirtualTextError guifg=#414868
+--   highlight DiagnosticVirtualTextWarn guifg=#414868
+--   highlight DiagnosticVirtualTextHint guifg=#414868
+--   highlight DiagnosticVirtualTextInfo guifg=#414868
 -- ]]
